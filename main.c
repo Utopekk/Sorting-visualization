e5c09b51cd1ad8b5391af0aed980c3b47bc5a6da
Utopekk <120266083+Utopekk@users.noreply.github.com>
2025-01-05 11:14:16 +0100

    Update main.c
    Added quickSort

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
+// Sorting Algorithm Visualizer
+// This program visualizes sorting algorithms (Bubble Sort, Selection Sort, and Quick Sort)
+// in a graphical interface using the Win32 API.
+
 #include <windows.h>
-#define WIDTH 800
+#define WIDTH 700
 #define HEIGHT 700
 
 int values[WIDTH];
@@ -10,10 +14,12 @@ void initializeValues() {
         values[i] = rand() % HEIGHT;
     }
 }
+
 void drawMenu(HDC hdc) {
-    const char* menuText = "Press '1' for Bubble Sort\nPress '2' for Selection Sort";
+    const char* menuText = "Press '1' for Bubble Sort\nPress '2' for Selection Sort\n Press '3' for Quick Sort\n";
     TextOut(hdc, 10, 10, menuText, strlen(menuText));
 }
+
 void draw(HDC backBufferDC,COLORREF color) {
     RECT clearRect = {0, 0, WIDTH, HEIGHT};
     FillRect(backBufferDC, &clearRect, (HBRUSH)(COLOR_WINDOW + 1));
@@ -26,6 +32,17 @@ void draw(HDC backBufferDC,COLORREF color) {
     }
 }
 
+void updateVisualization(HDC hdc, HDC backBufferDC, HBITMAP backBufferBitmap) {
+    draw(backBufferDC, RGB(0, 0, 255));
+    BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void bubbleSort(HDC hdc, int arr[], int len) {
     HDC backBufferDC = CreateCompatibleDC(hdc);
     HBITMAP backBufferBitmap = CreateCompatibleBitmap(hdc, WIDTH, HEIGHT);
@@ -34,14 +51,11 @@ void bubbleSort(HDC hdc, int arr[], int len) {
     for (int i = 0; i < len - 1; i++) {
         for (int j = 0; j < len - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(&arr[j],&arr[j+1]);
             }
+            updateVisualization(hdc, backBufferDC, backBufferBitmap);
         }
-        draw(backBufferDC,RGB(0,0,255));
-        Sleep(5);
-        BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
+
     }
     draw(backBufferDC, RGB(0, 255, 0));
     BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
@@ -59,20 +73,54 @@ void selectionSort(HDC hdc, int arr[], int len) {
         for (int j = i + 1; j < len; j++) {
             if (arr[j] < arr[current]) {
                 current = j;
+                updateVisualization(hdc, backBufferDC, backBufferBitmap);
             }
         }
-        int temp = arr[i];
-        arr[i] = arr[current];
-        arr[current] = temp;
+        swap(&arr[i], &arr[current]);
 
-        draw(backBufferDC,RGB(0,0,255));
-        Sleep(5);
-        BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
+
     }
     draw(backBufferDC, RGB(0, 255, 0));
     BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
     DeleteObject(backBufferBitmap);
     DeleteDC(backBufferDC);
+}
+
+int partition(HDC hdc, int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    HDC backBufferDC = CreateCompatibleDC(hdc);
+    HBITMAP backBufferBitmap = CreateCompatibleBitmap(hdc, WIDTH, HEIGHT);
+    SelectObject(backBufferDC, backBufferBitmap);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+        updateVisualization(hdc, backBufferDC, backBufferBitmap);
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+
+    draw(backBufferDC, RGB(0, 0, 255));
+    Sleep(5);
+    BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
+
+    DeleteObject(backBufferBitmap);
+    DeleteDC(backBufferDC);
+
+    return i + 1;
+}
+
+void quickSort(HDC hdc, int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(hdc, arr, low, high);
+
+        quickSort(hdc, arr, low, pivot - 1);
+        quickSort(hdc, arr, pivot + 1, high);
+    }
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -82,10 +130,15 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 sortChoice = 1;
                 InvalidateRect(hWnd, NULL, TRUE);
             }
-            if(wParam == '2'){
+            else if(wParam == '2'){
                 sortChoice = 2;
                 InvalidateRect(hWnd, NULL, TRUE);
             }
+            else if(wParam == '3'){
+                sortChoice = 3;
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+            break;
         case WM_CREATE: {
             initializeValues();
             return 0;
@@ -100,7 +153,18 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             if(sortChoice == 2) {
                 selectionSort(hdc, values, WIDTH);
             }
+            if(sortChoice == 3) {
+                quickSort(hdc, values, 0, WIDTH - 1);
+                HDC backBufferDC = CreateCompatibleDC(hdc);
+                HBITMAP backBufferBitmap = CreateCompatibleBitmap(hdc, WIDTH, HEIGHT);
+                SelectObject(backBufferDC, backBufferBitmap);
 
+                draw(backBufferDC, RGB(0, 255, 0));
+                BitBlt(hdc, 0, 0, WIDTH, HEIGHT, backBufferDC, 0, 0, SRCCOPY);
+
+                DeleteObject(backBufferBitmap);
+                DeleteDC(backBufferDC);
+            }
 
             EndPaint(hWnd, &ps);
             return 0;
